2376644d306c654d2784e5fb1e2abbaae38315b0
David <David.Wang@autoflight.com>
2018-09-05 15:02:27 +0800

    bookmark add and vimrc commit

--- 2018/eigen/src/hello_world.cpp
+++ 2018/eigen/src/hello_world.cpp
@@ -6,11 +6,39 @@ using namespace std;
 
 int main()
 {
-    MatrixXd m = MatrixXd::Random(3, 3);
-    m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
-    cout << m << endl;
-    VectorXd v(3);
-    v << 1, 2, 3;
-    cout << m * v << endl;
+    // 
+    {
+        MatrixXd m = MatrixXd::Random(3, 3);
+        VectorXd v(3);
+        m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
 
+        cout << m        << endl;
+        v    << 1, 2, 3;
+        cout << m * v    << endl;
+        v.resize(4);
+
+        cout << v << endl;
+    }
+
+    //
+    {
+        Matrix2d a;
+        a << 1, 2,
+             3, 4;
+        MatrixXd b(2,2);
+        b << 2, 3,
+             1, 4;
+
+        cout << "a + b =\n"     << a + b << endl;
+        cout << "a - b =\n"     << a - b << endl;
+        cout << "Doing a += b;" << endl;
+
+        a += b;
+        Vector3d v(1,2,3);
+        Vector3d w(1,0,0);
+
+        cout << "Now a =\n"      << a          << endl;
+        cout << "-v + w - v =\n" << -v + w - v << endl;
+
+    }
 }
