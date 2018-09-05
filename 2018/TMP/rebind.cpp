5b9c50468adcce186c16a2c5de4b2ef100d4b07f
David <David.Wang@autoflight.com>
2018-09-05 09:33:40 +0800

    template rebin

---
+++ 2018/TMP/rebind.cpp
@@ -0,0 +1,27 @@
+#include <iostream>  
+#include <ostream>  
+#include <typeinfo>  
+
+#define CONFORMANT
+
+using namespace std;  
+  
+template <typename T> struct Allocator {  
+    template <typename U> struct Rebind {  
+        typedef Allocator<U> Other;  
+    };  
+};  
+  
+template <typename X, typename AY> struct Container {  
+    #if defined(NONCONFORMANT)  
+        typedef typename AY::Rebind<X>::Other AX; // nonconformant  
+    #elif defined(CONFORMANT)  
+        typedef typename AY::template Rebind<X>::Other AX; // conformant  
+    #else  
+        #error Define NONCONFORMANT or CONFORMANT.  
+    #endif  
+};  
+  
+int main() {  
+    cout << typeid(Container<int, Allocator<float>>::AX).name() << endl;  
+}  
