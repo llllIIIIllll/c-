371b7708653de3bda888656e340d800100cbda69
David <David.Wang@autoflight.com>
2018-09-05 13:56:23 +0800

    new

--- 2018/TMP/constexpr_test.cpp
+++ 2018/TMP/constexpr_test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 template <typename T>
-constexpr T pi = T(3.1415926535897932385);template <typename T>T area_of_cirle_with_radius(T r){	return pi<T> * r * r;}struct matrix_constants 
+constexpr T pi = T(3.1415926535897932385);
+template <typename T>
+T area_of_cirle_with_radius(T r){ return pi<T> * r * r; }
+struct matrix_constants 
 {
 	template<typename T>
 	using pauli = hermitian_matrix<T, 2>;
@@ -11,5 +14,14 @@ constexpr T pi = T(3.1415926535897932385);template <typename T>T area_of_cirl
 	constexpr pauli<T> sigma2 = { { 0, -1i },{ 1i, 0 } };
 	template<typename T>
 	constexpr pauli<T> sigma3 = { { 1, 0 },{ -1, 0 } };
-};int main(){
-	using std::cout;	using std::endl;	using std::cin;	cout << area_of_cirle_with_radius<int>(1) << endl;	system("pause");}
+};
+
+int main()
+{
+	using std::cout;
+    using std::endl;
+    using std::cin;
+    cout << area_of_cirle_with_radius<int>(1) << endl;
+    system("pause");
+}
+
