58c8a871e70cf5035822d61ad26d8ae57d3fe4eb
David <David.Wang@autoflight.com>
2018-09-06 16:10:01 +0800

    vimrc change

--- 2018/eigen/src/hello_world.cpp
+++ 2018/eigen/src/hello_world.cpp
@@ -39,6 +39,16 @@ int main()
 
         cout << "Now a =\n"      << a          << endl;
         cout << "-v + w - v =\n" << -v + w - v << endl;
+    }
 
+    // table
+    {
+        ArrayXXf table(10, 4);
+        table.col(0) = ArrayXf::LinSpaced(10, 0, 90);
+        table.col(1) = M_PI / 180 * table.col(0);
+        table.col(2) = table.col(1).sin();
+        table.col(3) = table.col(1).cos();
+        cout << "  Degrees   Radians      Sine    Cosine\n";
+        cout << table << endl;
     }
 }
---
+++ 2018/eigen/src/linear.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main() 
+{
+    Matrix3f A;
+    Vector3f b;
+
+    A << 1, 2, 3, 4, 5, 6, 7, 8, 10;
+    b << 3, 3, 4;
+
+    cout << "A:\n" << A << endl;
+    cout << "b:\n" << b << endl;
+
+    Vector3f x = A.colPivHouseholderQr().solve(b);
+    cout << "Solution:\n" << x << endl;
+}
