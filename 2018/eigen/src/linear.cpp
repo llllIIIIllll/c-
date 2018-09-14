cb8ffd158308d72decc90ef762530a64f12141d7
David Wang <david.wang@autoflight.com>
2018-09-14 22:18:10 +0800

    vim

--- 2018/eigen/src/linear.cpp
+++ 2018/eigen/src/linear.cpp
@@ -13,9 +13,11 @@ int main()
     A << 1, 2, 3, 4, 5, 6, 7, 8, 10;
     b << 3, 3, 4;
 
-    cout << "A:\n" << A << endl;
-    cout << "b:\n" << b << endl;
+    cout << "A:\n"        << A << endl;
+    cout << "b:\n"        << b << endl;
+
 
     Vector3f x = A.colPivHouseholderQr().solve(b);
+
     cout << "Solution:\n" << x << endl;
 }
