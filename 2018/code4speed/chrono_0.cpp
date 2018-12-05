413e360a86cfb1aa2109edebc2bf1faa28df7427
llllIIIIllll <995513978@qq.com>
2018-12-05 09:45:58 +0800

    ssh change

--- 2018/code4speed/chrono_0.cpp
+++ 2018/code4speed/chrono_0.cpp
@@ -11,6 +11,7 @@ int main()
     {
         auto time_now_us = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch());
         long local_us =  time_now_us.count();
+        
         std::cout << local_us << std::endl;
         std::this_thread::sleep_for(std::chrono::microseconds(1));
     }
